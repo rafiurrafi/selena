9599b2628dda6f95085bf1343fc842a438a4d612
Rafiur Rahman <rrfoundary@gmail.com>
2021-06-12 20:10:34 +0600

    Added title animation

--- js/app.js
+++ js/app.js
@@ -1,9 +1,79 @@
 $(document).ready(function () {
-  const humbergar_menu = document.querySelector(".header__humbergar");
+  const humbergar_menu = document.querySelector(".header__humbergar .menu");
   const headerMiniNav = document.querySelector(".header-mini-nav");
+
   humbergar_menu.addEventListener("click", () => {
     headerMiniNav.classList.toggle("active");
+    console.log("Click");
   });
+
+  //animation on scroll
+  // $(".section-album").waypoint(
+  //   function (direction) {
+  //     $(".section-album").addClass("animate__animated animate__fadeIn");
+  //   },
+  //   { offset: "50%" }
+  // );
+  //animation on title album
+  $(".section-album .title").waypoint(
+    function (direction) {
+      $(".section-album .title h2").addClass(
+        "animate__animated animate__slideInLeft"
+      );
+      $(".section-album .title p").addClass(
+        "animate__animated animate__slideInRight"
+      );
+    },
+    { offset: "40%" }
+  );
+  //animation on title music
+  $(".section-music .title").waypoint(
+    function (direction) {
+      $(".section-music .title h2").addClass(
+        "animate__animated animate__slideInLeft"
+      );
+      $(".section-music .title p").addClass(
+        "animate__animated animate__slideInRight"
+      );
+    },
+    { offset: "40%" }
+  );
+  //animation on title tour
+  $(".section-tour .title").waypoint(
+    function (direction) {
+      $(".section-tour .title h2").addClass(
+        "animate__animated animate__slideInLeft"
+      );
+      $(".section-tour .title p").addClass(
+        "animate__animated animate__slideInRight"
+      );
+    },
+    { offset: "40%" }
+  );
+  //animation on title video
+  $(".section-video .title").waypoint(
+    function (direction) {
+      $(".section-video .title h2").addClass(
+        "animate__animated animate__slideInLeft"
+      );
+      $(".section-video .title p").addClass(
+        "animate__animated animate__slideInRight"
+      );
+    },
+    { offset: "40%" }
+  );
+  //animation on title merch
+  $(".section-merch .title").waypoint(
+    function (direction) {
+      $(".section-merch .title h2").addClass(
+        "animate__animated animate__slideInLeft"
+      );
+      $(".section-merch .title p").addClass(
+        "animate__animated animate__slideInRight"
+      );
+    },
+    { offset: "40%" }
+  );
 
   // Select all links with hashes
   $('a[href*="#"]')
